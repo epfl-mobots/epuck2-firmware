248573c88a9ee6199707afbcaadbea16f44bcd61
Thibault Alcouffe <thibault.alcouffe@epfl.ch>
2015-03-27 15:47:53 +0100

    started implementing ADC demo

--- src/sensors/proximity.c
+++ src/sensors/proximity.c
@@ -1,10 +1,61 @@
 #include "proximity.h"
 
-#include <ch.h>
-#include "proximity.h"
+#include "ch.h"
+#include "hal.h"
 #include "tcrt1000.h"
 
 
+#define ADC_GRP1_NUM_CHANNELS   1
+#define ADC_GRP1_BUF_DEPTH      1
 
-palReadPad(port, pad);
 
+static adcsample_t samples1[ADC_GRP1_NUM_CHANNELS * ADC_GRP1_BUF_DEPTH];
+
+
+
+/*
+ * ADC streaming callback.
+ */
+size_t nx = 0, ny = 0;
+static void adccallback(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
+
+  (void)adcp;
+  if (samples1 == buffer) {
+    nx += n;
+  }
+  else {
+    ny += n;
+  }
+}
+
+static void adcerrorcallback(ADCDriver *adcp, adcerror_t err) {
+
+  (void)adcp;
+  (void)err;
+}
+
+
+/*
+ * ADC conversion group.
+ * Mode:        Linear buffer, 8 samples of 1 channel, SW triggered.
+ * Channels:    IN11.
+ */
+
+static const ADCConversionGroup adcgrpcfg1 = {
+  FALSE,
+  ADC_GRP1_NUM_CHANNELS,
+  NULL,
+  adcerrorcallback,
+  0,                        /* CR1 */
+  ADC_CR2_SWSTART,          /* CR2 */
+  ADC_SMPR1_SMP_AN11(ADC_SAMPLE_3),
+  0,                        /* SMPR2 */
+  ADC_SQR1_NUM_CH(ADC_GRP1_NUM_CHANNELS),
+  0,		                 /* SQR2 */
+  ADC_SQR3_SQ1_N(ADC_CHANNEL_IN11)
+};
+
+
+int proximity_main(void) {
+	return 0;
+}
--- src/src.mk
+++ src/src.mk
@@ -1,1 +1,1 @@
-SRC = src/main.c src/mpu60X0.c src/usbcfg.c
+SRC = src/main.c src/mpu60X0.c src/usbcfg.c src/sensors/proximity.c
