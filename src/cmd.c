084356f7391a04362cc4271a3174510bd621358a
Patrick Spieler <stapelzeiger@gmail.com>
2015-03-30 20:09:12 +0200

    cmd.c coding style

--- src/cmd.c
+++ src/cmd.c
@@ -4,63 +4,66 @@
 #include "shell.h"
 #include "usbcfg.h"
 
-#define TEST_WA_SIZE    THD_WORKING_AREA_SIZE(256)
+#define TEST_WA_SIZE        THD_WORKING_AREA_SIZE(256)
 
-static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]) {
-  size_t n, size;
+static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    size_t n, size;
 
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: mem\r\n");
-    return;
-  }
-  n = chHeapStatus(NULL, &size);
-  chprintf(chp, "core free memory : %u bytes\r\n", chCoreGetStatusX());
-  chprintf(chp, "heap fragments   : %u\r\n", n);
-  chprintf(chp, "heap free total  : %u bytes\r\n", size);
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: mem\r\n");
+        return;
+    }
+    n = chHeapStatus(NULL, &size);
+    chprintf(chp, "core free memory : %u bytes\r\n", chCoreGetStatusX());
+    chprintf(chp, "heap fragments     : %u\r\n", n);
+    chprintf(chp, "heap free total    : %u bytes\r\n", size);
 }
 
-static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
-  static const char *states[] = {CH_STATE_NAMES};
-  thread_t *tp;
+static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    static const char *states[] = {CH_STATE_NAMES};
+    thread_t *tp;
 
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: threads\r\n");
-    return;
-  }
-  chprintf(chp, "    addr    stack prio refs     state\r\n");
-  tp = chRegFirstThread();
-  do {
-    chprintf(chp, "%08lx %08lx %4lu %4lu %9s\r\n",
-             (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
-             (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-             states[tp->p_state]);
-    tp = chRegNextThread(tp);
-  } while (tp != NULL);
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: threads\r\n");
+        return;
+    }
+    chprintf(chp, "        addr        stack prio refs         state\r\n");
+    tp = chRegFirstThread();
+    do {
+        chprintf(chp, "%08lx %08lx %4lu %4lu %9s\r\n",
+                         (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
+                         (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
+                         states[tp->p_state]);
+        tp = chRegNextThread(tp);
+    } while (tp != NULL);
 }
 
-static void cmd_test(BaseSequentialStream *chp, int argc, char *argv[]) {
-  thread_t *tp;
+static void cmd_test(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    thread_t *tp;
 
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: test\r\n");
-    return;
-  }
-  tp = chThdCreateFromHeap(NULL, TEST_WA_SIZE, chThdGetPriorityX(),
-                           TestThread, chp);
-  if (tp == NULL) {
-    chprintf(chp, "out of memory\r\n");
-    return;
-  }
-  chThdWait(tp);
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: test\r\n");
+        return;
+    }
+    tp = chThdCreateFromHeap(NULL, TEST_WA_SIZE, chThdGetPriorityX(),
+                                                     TestThread, chp);
+    if (tp == NULL) {
+        chprintf(chp, "out of memory\r\n");
+        return;
+    }
+    chThdWait(tp);
 }
 
 
 const ShellCommand shell_commands[] = {
-  {"mem", cmd_mem},
-  {"threads", cmd_threads},
-  {"test", cmd_test},
-  {NULL, NULL}
+    {"mem", cmd_mem},
+    {"threads", cmd_threads},
+    {"test", cmd_test},
+    {NULL, NULL}
 };
