109363793019fb2e410bdf670b31fc3091c58364
Thibault Alcouffe <thibault.alcouffe@epfl.ch>
2015-03-30 21:59:25 +0200

    finished configuring test conversion group

--- src/sensors/proximity.c
+++ src/sensors/proximity.c
@@ -14,45 +14,23 @@ static adcsample_t samples1[ADC_GRP1_NUM_CHANNELS * ADC_GRP1_BUF_DEPTH];
 
 
 /*
- * ADC streaming callback.
- */
-size_t nx = 0, ny = 0;
-static void adccallback(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
-
-  (void)adcp;
-  if (samples1 == buffer) {
-    nx += n;
-  }
-  else {
-    ny += n;
-  }
-}
-
-static void adcerrorcallback(ADCDriver *adcp, adcerror_t err) {
-
-  (void)adcp;
-  (void)err;
-}
-
-
-/*
  * ADC conversion group.
- * Mode:        Linear buffer, 8 samples of 1 channel, SW triggered.
- * Channels:    IN11.
+ * Mode:        Continuous buffer, 1 channel, SW triggered, ADC-Prescaler 480
+ * Channels:    IN2.
  */
 
 static const ADCConversionGroup adcgrpcfg1 = {
-  FALSE,
+  TRUE,
   ADC_GRP1_NUM_CHANNELS,
   NULL,
-  adcerrorcallback,
+  NULL,
   0,                        /* CR1 */
   ADC_CR2_SWSTART,          /* CR2 */
-  ADC_SMPR2_SMP_AN2(ADC_SAMPLE_3),
+  ADC_SMPR2_SMP_AN2(ADC_SAMPLE_480),
   0,                        /* SMPR2 */
   ADC_SQR1_NUM_CH(ADC_GRP1_NUM_CHANNELS),
   0,		                 /* SQR2 */
-  ADC_SQR3_SQ1_N(ADC_CHANNEL_IN11)
+  ADC_SQR3_SQ1_N(ADC_CHANNEL_IN2)
 };
 
 
