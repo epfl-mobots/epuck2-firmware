31e16c185f0ac882a4bcbc81394124f268306e84
Thibault Alcouffe <thibault.alcouffe@epfl.ch>
2015-04-06 16:04:34 +0200

    changed indentation to spacing

--- src/sensors/imu.h
+++ src/sensors/imu.h
@@ -21,7 +21,7 @@ typedef struct {
 	float rate[3];
 } accelerometer_sample_t;
 
-extern gyrometer_sample_t 		imu_gyro_sample;
-extern accelerometer_sample_t 	imu_acc_sample;
+extern gyrometer_sample_timu_   gyro_sample;
+extern accelerometer_sample_t   imu_acc_sample;
 
 #endif /* IMU_H */
