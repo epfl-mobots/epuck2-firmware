b0370671486f723a36904be1a478f92fc125a801
Thibault Alcouffe <thibault.alcouffe@epfl.ch>
2015-05-18 09:16:30 +0200

    removed any features from the main.c

--- src/main.c
+++ src/main.c
@@ -15,45 +15,8 @@
 
 
 int main(void) {
-    thread_t *shelltp = NULL;
 
     halInit();
     chSysInit();
 
-    // UART2 on PA2(TX) and PA3(RX)
-    sdStart(&SD2, NULL);
-    palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7));
-    palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7));
-
-    // serial-over-USB CDC driver.
-    sduObjectInit(&SDU1);
-    sduStart(&SDU1, &serusbcfg);
-    usbDisconnectBus(serusbcfg.usbp);
-    chThdSleepMilliseconds(1000);
-    usbStart(serusbcfg.usbp, &usbcfg);
-    usbConnectBus(serusbcfg.usbp);
-
-    imu_init();
-    imu_start();
-
-    shellInit();
-
-    static const ShellConfig shell_cfg1 = {
-        (BaseSequentialStream *)&SDU1,
-        shell_commands
-    };
-
-    while (TRUE) {
-        if (!shelltp) {
-            if (SDU1.config->usbp->state == USB_ACTIVE) {
-                shelltp = shellCreate(&shell_cfg1, SHELL_WA_SIZE, NORMALPRIO);
-            }
-        } else {
-            if (chThdTerminatedX(shelltp)) {
-                chThdRelease(shelltp);
-                shelltp = NULL;
-            }
-        }
-        chThdSleepMilliseconds(500);
-    }
 }
