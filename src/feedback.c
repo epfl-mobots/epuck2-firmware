556c34036f2372a2c3a61f8e3139ff87991df200
Thibault Alcouffe <thibault.alcouffe@epfl.ch>
2015-05-26 18:21:25 +0200

    feedback now gives back raw encoder.

--- src/feedback.c
+++ src/feedback.c
@@ -5,11 +5,17 @@
 
 void feedback_get (feedback *left, feedback *right)
 {
-	left->position = 0;
-	right->position = 0;
+	int buff_left = 0;
+	int buff_right = 0;
 
-	left->velocity = 0;
-	right->velocity = 0;
+	buff_left = encoder_get_left();
+	buff_right = encoder_get_right();
+
+	left->velocity = (float) encoder_tick_diff(left->position, buff_left);
+	right->velocity = (float) encoder_tick_diff(right->position, buff_right);
+
+	left->position = (float) encoder_get_left();
+	right->position = (float) encoder_get_right();
 
 	left->current = 0;
 	right->current = 0;
