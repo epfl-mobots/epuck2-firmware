cba7205f51326884d9051d31e8a94d22169dcfc8
Antoine Albertelli <antoine@antoinealb.net>
2016-04-20 18:36:06 +0200

    Delete deadcode and put infinite loop in panic handler

--- src/panic.c
+++ src/panic.c
@@ -3,30 +3,10 @@
 
 void panic_handler(const char *reason)
 {
-    (void)reason;
+    (void) reason;
 
-    //Turns on LED_ERROR
     palSetPad(GPIOD, GPIOD_LED_ERROR);
 
-    // static volatile uint32_t ipsr;
-    // static volatile const char *msg;
-    // msg = reason;
-    // ipsr = __get_IPSR();
-
-    // (void)msg;
-    // (void)ipsr;
-
-    // strncpy(panic_buffer, reason, sizeof(panic_buffer));
-
-    // panic_buffer[sizeof(panic_buffer) - 1] = '\0';
-    // panic_buffer_crc = crc32(PANIC_CRC_INIT, panic_buffer, sizeof(panic_buffer));
-
-// #ifdef DEBUG // debug builds block to be able to connect a debugger
-//     while (1) {
-//         // todo print error over uart
-//     }
-
-// #else // non-debug builds reboot in safemode
-//     reboot_in_safemode();
-// #endif
+    while (1) {
+    }
 }
