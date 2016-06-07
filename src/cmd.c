0d581834764bcdf6bea3bab3a6e326327f482ddd
Antoine Albertelli <antoine@antoinealb.net>
2016-06-07 17:24:07 +0200

    Removed unused commands

--- src/cmd.c
+++ src/cmd.c
@@ -11,42 +11,6 @@
 #define TEST_WA_SIZE        THD_WORKING_AREA_SIZE(256)
 #define SHELL_WA_SIZE       THD_WORKING_AREA_SIZE(2048)
 
-static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[])
-{
-    size_t n, size;
-
-    (void)argv;
-    if (argc > 0) {
-        chprintf(chp, "Usage: mem\r\n");
-        return;
-    }
-    n = chHeapStatus(NULL, &size);
-    chprintf(chp, "core free memory : %u bytes\r\n", chCoreGetStatusX());
-    chprintf(chp, "heap fragments     : %u\r\n", n);
-    chprintf(chp, "heap free total    : %u bytes\r\n", size);
-}
-
-static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[])
-{
-    static const char *states[] = {CH_STATE_NAMES};
-    thread_t *tp;
-
-    (void)argv;
-    if (argc > 0) {
-        chprintf(chp, "Usage: threads\r\n");
-        return;
-    }
-    chprintf(chp, "        addr        stack prio refs         state\r\n");
-    tp = chRegFirstThread();
-    do {
-        chprintf(chp, "%08lx %08lx %4lu %4lu %9s\r\n",
-                 (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
-                 (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-                 states[tp->p_state]);
-        tp = chRegNextThread(tp);
-    } while (tp != NULL);
-}
-
 static void cmd_test(BaseSequentialStream *chp, int argc, char *argv[])
 {
     thread_t *tp;
@@ -83,14 +47,6 @@ static void cmd_mpu6050(BaseSequentialStream *chp, int argc, char *argv[])
              acc[2]);
 }
 
-static void cmd_readclock(BaseSequentialStream *chp, int argc, char *argv[])
-{
-    (void)argc;
-    (void)argv;
-    chprintf(chp, "SYSCLK: %i \n HCLK: %i \n PCLK1  %i \n PCLK2 %i \n",
-             STM32_SYSCLK, STM32_HCLK, STM32_PCLK1, STM32_PCLK2);
-}
-
 static void cmd_motor(BaseSequentialStream *chp, int argc, char *argv[])
 {
     if (argc < 2) {
@@ -115,12 +71,9 @@ static void cmd_motor(BaseSequentialStream *chp, int argc, char *argv[])
 
 
 const ShellCommand shell_commands[] = {
-    {"mem", cmd_mem},
-    {"threads", cmd_threads},
     {"test", cmd_test},
     {"pwm", cmd_motor},
     {"mpu6050", cmd_mpu6050},
-    {"clock", cmd_readclock},
     {NULL, NULL}
 };
 
@@ -155,7 +108,7 @@ static THD_FUNCTION(shell_spawn_thd, p)
 
 void shell_start(void)
 {
-    static THD_WORKING_AREA(wa, 2048);
+    static THD_WORKING_AREA(wa, SHELL_WA_SIZE);
 
     chThdCreateStatic(wa, sizeof(wa), NORMALPRIO, shell_spawn_thd, NULL);
 }
