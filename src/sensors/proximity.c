e3fa88880196dc287ce7380d44aeeda9b0bd1b63
Antoine Albertelli <antoine@antoinealb.net>
2016-06-28 10:29:03 +0200

    Add support for the TCRT1000 on ADC2.

--- src/sensors/proximity.c
+++ src/sensors/proximity.c
@@ -19,8 +19,55 @@
 #define PROXIMITY_ADC_SAMPLE_TIME ADC_SAMPLE_112
 #define DMA_BUFFER_SIZE (16)
 
-unsigned int values[PROXIMITY_NB_CHANNELS] = {0};
-static BSEMAPHORE_DECL(measurement_ready, true);
+#define ADC2_NB_CHANNELS 1
+
+unsigned int adc3_values[PROXIMITY_NB_CHANNELS] = {0};
+unsigned int adc2_values[ADC2_NB_CHANNELS] = {0};
+static BSEMAPHORE_DECL(adc3_ready, true);
+static BSEMAPHORE_DECL(adc2_ready, true);
+
+static adcsample_t adc3_proximity_samples[PROXIMITY_NB_CHANNELS * DMA_BUFFER_SIZE];
+static adcsample_t adc2_proximity_samples[1 * DMA_BUFFER_SIZE];
+
+static void adc2_cb(ADCDriver *adcp, adcsample_t *samples, size_t n)
+{
+    memset(adc2_values, 0, sizeof(adc2_values));
+
+    for (size_t j = 0; j < n; j++) {
+        for (size_t i = 0; i < ADC2_NB_CHANNELS; i++) {
+            adc2_values[i] += samples[ADC2_NB_CHANNELS * j + i];
+        }
+    }
+
+    for (size_t i = 0; i < ADC2_NB_CHANNELS; i++) {
+        adc2_values[i] /= n;
+    }
+
+    chSysLockFromISR();
+    adcStopConversionI(&ADCD2);
+    chBSemSignalI(&adc2_ready);
+    chSysUnlockFromISR();
+
+}
+
+static const ADCConversionGroup adcgrpcfg2 = {
+    .circular = true,
+    .num_channels = ADC2_NB_CHANNELS,
+    .end_cb = adc2_cb,
+    .error_cb = NULL,
+
+    /* Discontinuous mode with 1 conversion per trigger. */
+    .cr1 = ADC_CR1_DISCEN,
+
+    /* Trigger on timer 8 CC1. */
+    .cr2 = ADC_CR2_EXTEN_1 | ADC_CR2_EXTSEL_SRC(0xd),
+    .smpr1 = 0,
+    .smpr2 = ADC_SMPR2_SMP_AN0(PROXIMITY_ADC_SAMPLE_TIME),
+
+    .sqr1 = ADC_SQR1_NUM_CH(ADC2_NB_CHANNELS),
+    .sqr2 = 0,
+    .sqr3 = ADC_SQR3_SQ1_N(14), // IR_AN12
+};
 
 static void adc3_proximity_cb(ADCDriver *adcp, adcsample_t *samples, size_t n)
 {
@@ -28,92 +75,95 @@ static void adc3_proximity_cb(ADCDriver *adcp, adcsample_t *samples, size_t n)
     (void) samples;
     (void) n;
 
-    memset(values, 0, sizeof(values));
+    memset(adc3_values, 0, sizeof(adc3_values));
 
     for (size_t j = 0; j < n; j++) {
         for (size_t i = 0; i < PROXIMITY_NB_CHANNELS; i++) {
-            values[i] += samples[PROXIMITY_NB_CHANNELS * j + i];
+            adc3_values[i] += samples[PROXIMITY_NB_CHANNELS * j + i];
         }
     }
 
     for (size_t i = 0; i < PROXIMITY_NB_CHANNELS; i++) {
-        values[i] /= n;
+        adc3_values[i] /= n;
     }
 
     palTogglePad(GPIOE, GPIOE_LED_STATUS);
 
     chSysLockFromISR();
-    chBSemSignalI(&measurement_ready);
+    adcStopConversionI(&ADCD3);
+    chBSemSignalI(&adc3_ready);
     chSysUnlockFromISR();
 }
+
+static const ADCConversionGroup adcgrpcfg3 = {
+    .circular = true,
+    .num_channels = PROXIMITY_NB_CHANNELS,
+    .end_cb = adc3_proximity_cb,
+    .error_cb = NULL,
+
+    /* Discontinuous mode with 1 conversion per trigger. */
+    .cr1 = ADC_CR1_DISCEN,
+
+    /* Trigger on timer 8 CC1. */
+    .cr2 = ADC_CR2_EXTEN_1 | ADC_CR2_EXTSEL_SRC(0xd),
+
+    .smpr1 = ADC_SMPR1_SMP_AN10(PROXIMITY_ADC_SAMPLE_TIME) |  // PC0 - IR_AN8
+        ADC_SMPR1_SMP_AN11(PROXIMITY_ADC_SAMPLE_TIME) | // PC1 - IR_AN9
+        ADC_SMPR1_SMP_AN12(PROXIMITY_ADC_SAMPLE_TIME) | // PC2 - IR_AN10
+        ADC_SMPR1_SMP_AN13(PROXIMITY_ADC_SAMPLE_TIME) | // PC3 - IR_AN11
+        ADC_SMPR1_SMP_AN14(PROXIMITY_ADC_SAMPLE_TIME) | // PF4 - IR_AN1
+        ADC_SMPR1_SMP_AN15(PROXIMITY_ADC_SAMPLE_TIME), // PF5 - IR_AN2
+
+    .smpr2 =
+        ADC_SMPR2_SMP_AN0(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
+        ADC_SMPR2_SMP_AN1(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
+        ADC_SMPR2_SMP_AN2(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
+        ADC_SMPR2_SMP_AN3(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
+        ADC_SMPR2_SMP_AN4(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
+        ADC_SMPR2_SMP_AN5(PROXIMITY_ADC_SAMPLE_TIME) | // PF7 - IR_AN4
+        ADC_SMPR2_SMP_AN6(PROXIMITY_ADC_SAMPLE_TIME) | // PF8 - IR_AN5
+        ADC_SMPR2_SMP_AN7(PROXIMITY_ADC_SAMPLE_TIME) | // PF9 - IR_AN6
+        ADC_SMPR2_SMP_AN8(PROXIMITY_ADC_SAMPLE_TIME) | // PF10 - IR_AN7
+        ADC_SMPR2_SMP_AN9(PROXIMITY_ADC_SAMPLE_TIME), // PF3 - IR_AN0
+
+    // Proximity sensors channels (CCW, from above, front is range sensor)
+    // 12
+    // 13
+    // 11
+    // 14
+    // 15
+    // 6
+    // 5
+
+    // Ground sensors
+    // 8
+    // ADC2, PC4, IN14 (See below)
+    // 10
+    // 4
+    // 7
+    // 9
+
+    .sqr1 = ADC_SQR1_NUM_CH(PROXIMITY_NB_CHANNELS) | ADC_SQR1_SQ13_N(9),
+    /*SQR2*/
+    .sqr2 = ADC_SQR2_SQ7_N(5) | ADC_SQR2_SQ8_N(8) | ADC_SQR2_SQ9_N(2) | ADC_SQR2_SQ10_N(10) |
+        ADC_SQR2_SQ11_N(4) | ADC_SQR2_SQ12_N(7),
+    /*SQR3*/
+    .sqr3 = ADC_SQR3_SQ1_N(12) | ADC_SQR3_SQ2_N(13) |
+        ADC_SQR3_SQ3_N(11) | ADC_SQR3_SQ4_N(14) |
+        ADC_SQR3_SQ5_N(15) | ADC_SQR3_SQ6_N(6),
+};
 
 static THD_FUNCTION(proximity_thd, arg)
 {
     (void) arg;
     chRegSetThreadName(__FUNCTION__);
 
-    static adcsample_t adc3_proximity_samples[PROXIMITY_NB_CHANNELS * DMA_BUFFER_SIZE];
-    static const ADCConversionGroup adcgrpcfg3 = {
-        .circular = true,
-        .num_channels = PROXIMITY_NB_CHANNELS,
-        .end_cb = adc3_proximity_cb,
-        .error_cb = NULL,
-
-        /* Discontinuous mode with 8 conversion per trigger. */
-        .cr1 = ADC_CR1_DISCEN | ADC_CR1_DISCNUM_0,
-
-        /* Trigger on timer 8 CC1. */
-        .cr2 = ADC_CR2_EXTEN_1 | ADC_CR2_EXTSEL_SRC(0xd),
-
-        .smpr1 = ADC_SMPR1_SMP_AN10(PROXIMITY_ADC_SAMPLE_TIME) |  // PC0 - IR_AN8
-                 ADC_SMPR1_SMP_AN11(PROXIMITY_ADC_SAMPLE_TIME) | // PC1 - IR_AN9
-                 ADC_SMPR1_SMP_AN12(PROXIMITY_ADC_SAMPLE_TIME) | // PC2 - IR_AN10
-                 ADC_SMPR1_SMP_AN13(PROXIMITY_ADC_SAMPLE_TIME) | // PC3 - IR_AN11
-                 ADC_SMPR1_SMP_AN14(PROXIMITY_ADC_SAMPLE_TIME) | // PF4 - IR_AN1
-                 ADC_SMPR1_SMP_AN15(PROXIMITY_ADC_SAMPLE_TIME), // PF5 - IR_AN2
-
-        .smpr2 =
-            ADC_SMPR2_SMP_AN0(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
-            ADC_SMPR2_SMP_AN1(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
-            ADC_SMPR2_SMP_AN2(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
-            ADC_SMPR2_SMP_AN3(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
-            ADC_SMPR2_SMP_AN4(PROXIMITY_ADC_SAMPLE_TIME) | // PF6 - IR_AN3
-            ADC_SMPR2_SMP_AN5(PROXIMITY_ADC_SAMPLE_TIME) | // PF7 - IR_AN4
-            ADC_SMPR2_SMP_AN6(PROXIMITY_ADC_SAMPLE_TIME) | // PF8 - IR_AN5
-            ADC_SMPR2_SMP_AN7(PROXIMITY_ADC_SAMPLE_TIME) | // PF9 - IR_AN6
-            ADC_SMPR2_SMP_AN8(PROXIMITY_ADC_SAMPLE_TIME) | // PF10 - IR_AN7
-            ADC_SMPR2_SMP_AN9(PROXIMITY_ADC_SAMPLE_TIME), // PF3 - IR_AN0
-
-        // Proximity sensors channels (CCW, from above, front is range sensor)
-        // 12
-        // 13
-        // 11
-        // 14
-        // 15
-        // 6
-        // 5
-
-        // Ground sensors
-        // 8
-        // ADC2, TODO
-        // 10
-        // 4
-        // 7
-        // 9
-
-        .sqr1 = ADC_SQR1_NUM_CH(PROXIMITY_NB_CHANNELS) | ADC_SQR1_SQ13_N(9),
-        /*SQR2*/
-        .sqr2 = ADC_SQR2_SQ7_N(5) | ADC_SQR2_SQ8_N(8) | ADC_SQR2_SQ9_N(2) | ADC_SQR2_SQ10_N(10) |
-                ADC_SQR2_SQ11_N(4) | ADC_SQR2_SQ12_N(7),
-        /*SQR3*/
-        .sqr3 = ADC_SQR3_SQ1_N(12) | ADC_SQR3_SQ2_N(13) |
-                ADC_SQR3_SQ3_N(11) | ADC_SQR3_SQ4_N(14) |
-                ADC_SQR3_SQ5_N(15) | ADC_SQR3_SQ6_N(6),
-    };
-
-    /* Configure the AD converter. */
+    /* Configure the AD converters. */
     adcStart(&ADCD3, NULL);
+    adcStart(&ADCD2, NULL);
+
     adcStartConversion(&ADCD3, &adcgrpcfg3, adc3_proximity_samples, DMA_BUFFER_SIZE);
+    adcStartConversion(&ADCD2, &adcgrpcfg2, adc2_proximity_samples, DMA_BUFFER_SIZE);
 
     /* Declares the topic on the bus. */
     messagebus_topic_t proximity_topic;
@@ -130,10 +180,12 @@ static THD_FUNCTION(proximity_thd, arg)
 
     while (true) {
         unsigned int myvalues[PROXIMITY_NB_CHANNELS];
-        chBSemWait(&measurement_ready);
+
+        chBSemWait(&adc3_ready);
+        chBSemWait(&adc2_ready);
 
         chSysLock();
-        memcpy(myvalues, values, sizeof(myvalues));
+        memcpy(myvalues, adc3_values, sizeof(myvalues));
         chSysUnlock();
 
         proximity_msg_t msg;
@@ -142,10 +194,17 @@ static THD_FUNCTION(proximity_thd, arg)
             msg.values[i] = myvalues[i];
         }
 
-        /* TODO This sensor is on ADC2 and not implemented yet. */
-        msg.values[8] = 0;
+        /* This sensor is on ADC2 so it is slightly different. */
+        chSysLock();
+        msg.values[8] = adc2_values[0];
+        chSysUnlock();
 
         messagebus_topic_publish(&proximity_topic, &msg, sizeof(msg));
+
+        /* Starts a new conversion. */
+        adcStartConversion(&ADCD3, &adcgrpcfg3, adc3_proximity_samples, DMA_BUFFER_SIZE);
+        adcStartConversion(&ADCD2, &adcgrpcfg2, adc2_proximity_samples, DMA_BUFFER_SIZE);
+
     }
 }
 
@@ -170,7 +229,7 @@ void proximity_start(void)
                      {.mode = PWM_COMPLEMENTARY_OUTPUT_ACTIVE_HIGH, .callback = NULL},
                      {.mode = PWM_OUTPUT_DISABLED, .callback = NULL},
                      {.mode = PWM_OUTPUT_DISABLED, .callback = NULL},
-},
+        },
     };
 
     /* Init PWM */
