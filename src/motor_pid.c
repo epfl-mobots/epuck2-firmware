720ae02153259eb1a89a68e2fd7f8541d7b222e8
Florian Reinhard <florian.reinhard@epfl.ch>
2016-11-01 09:09:35 +0100

    motor PID parameters helper functions

--- src/motor_pid.c
+++ src/motor_pid.c
@@ -6,6 +6,28 @@
 #include "sensors/battery_level.h"
 #include "msgbus/messagebus.h"
 #include "main.h"
+#include "parameter/parameter.h"
+#include <math.h>
+
+
+struct pid_param_s {
+    parameter_t kp;
+    parameter_t ki;
+    parameter_t kd;
+    parameter_t i_limit;
+};
+
+// control loop parameters
+static parameter_namespace_t param_ns_control;
+static parameter_t param_vel_limit;
+static parameter_t param_torque_limit;
+static parameter_t param_acc_limit;
+static parameter_namespace_t param_ns_pos_ctrl;
+static parameter_namespace_t param_ns_vel_ctrl;
+static parameter_namespace_t param_ns_cur_ctrl;
+static struct pid_param_s pos_pid_params;
+static struct pid_param_s vel_pid_params;
+static struct pid_param_s cur_pid_params;
 
 
 static void right_wheel_voltage_set(float voltage)
@@ -60,6 +82,47 @@ static void left_wheel_voltage_set(float voltage)
         voltage_msg.left = voltage;
         messagebus_topic_publish(voltage_topic, &voltage_msg, sizeof(voltage_msg));
     }
+}
+
+static void pid_param_declare(struct pid_param_s *p, parameter_namespace_t *ns)
+{
+    parameter_scalar_declare_with_default(&p->kp, ns, "kp", 0);
+    parameter_scalar_declare_with_default(&p->ki, ns, "ki", 0);
+    parameter_scalar_declare_with_default(&p->kd, ns, "kd", 0);
+    parameter_scalar_declare_with_default(&p->i_limit, ns, "i_limit", INFINITY);
+}
+
+static void pid_param_update(struct pid_param_s *p, pid_ctrl_t *ctrl)
+{
+    if (parameter_changed(&p->kp) ||
+        parameter_changed(&p->ki) ||
+        parameter_changed(&p->kd)) {
+        pid_set_gains(ctrl, parameter_scalar_get(&p->kp),
+                            parameter_scalar_get(&p->ki),
+                            parameter_scalar_get(&p->kd));
+        pid_reset_integral(ctrl);
+    }
+    if (parameter_changed(&p->i_limit)) {
+        pid_set_integral_limit(ctrl, parameter_scalar_get(&p->i_limit));
+    }
+}
+
+
+static void declare_parameters(void)
+{
+    parameter_namespace_declare(&param_ns_control, &parameter_root, "control");
+    parameter_scalar_declare(&param_vel_limit, &param_ns_control, "velocity_limit");
+    parameter_scalar_declare(&param_torque_limit, &param_ns_control, "torque_limit");
+    parameter_scalar_declare(&param_acc_limit, &param_ns_control, "acceleration_limit");
+
+    parameter_namespace_declare(&param_ns_pos_ctrl, &param_ns_control, "position");
+    pid_param_declare(&pos_pid_params, &param_ns_pos_ctrl);
+
+    parameter_namespace_declare(&param_ns_vel_ctrl, &param_ns_control, "velocity");
+    pid_param_declare(&vel_pid_params, &param_ns_vel_ctrl);
+
+    parameter_namespace_declare(&param_ns_cur_ctrl, &param_ns_control, "current");
+    pid_param_declare(&cur_pid_params, &param_ns_cur_ctrl);
 }
 
 static THD_FUNCTION(motor_pid_thd, arg)
